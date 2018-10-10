ec4994d4551dc50c9aa7fdae978e75569ae3dd91
Jack Keogh <keeeogh@gmail.com>
2018-10-10 11:54:14 +0100

    ECS Entity class created

--- Entity-Component-System/Entity-Component-System/ECS.h
+++ Entity-Component-System/Entity-Component-System/ECS.h
@@ -14,6 +14,7 @@
 #pragma once
 #include <bitset>
 #include <array>
+#include <vector>
 
 namespace jk
 {
@@ -93,5 +94,66 @@ namespace jk
 		virtual void Render() {};
 
 		Entity * m_entity;
+	};
+
+	/// <summary>
+	/// An Entity Object that will be able be grouped into
+	/// predefined groups and layers. An entity will be made
+	/// up of components.
+	/// </summary>
+	class Entity
+	{
+	public:
+		// Constructors
+		Entity(EntityManager & Manager) :
+			m_manager(Manager), m_active(true) {};
+
+		// Destructors
+		~Entity() {};
+
+		/// <summary>
+		/// Used to call the update method for each of
+		/// the entity's component.
+		/// </summary>
+		void Update()
+		{
+			for (auto& c : m_components)
+			{
+				c->Update();
+			}
+		}
+
+		/// <summary>
+		/// Used to call the render method for each of
+		/// the entity's component.
+		/// </summary>
+		void Render()
+		{
+			for (auto& c : m_components)
+			{
+				c->Render();
+			}
+		}
+
+		/// <summary>
+		/// Returns a bool variable checking if the Entity is active or not.
+		/// </summary>
+		bool isActive() { return m_active; };
+
+		/// <summary>
+		/// Set if the Entity is active or not.
+		/// </summary>
+		/// <param name="set">A bool variable to set active to.</param>
+		void setActive(bool set) { m_active = set; };
+
+	private:
+		EntityManager & m_manager;
+		bool m_active;
+		std::vector<std::unique_ptr<Component>> m_components;
+		
+		ComponentArray m_componentArray;
+		ComponentBitSet m_componentBitSet;
+		GroupBitSet m_groupBitSet;
+		LayerBitSet m_layerBitSet;
 	};
 }
